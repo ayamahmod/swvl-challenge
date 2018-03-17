73af5ee6294153a8d7a4a0eb7da930e131469bed
ayamahmod <ayamahmod1193@gmail.com>
2018-03-17 21:02:36 +0200

    Linting & Adding comments

--- authorization-control.js
+++ authorization-control.js
@@ -1,231 +1,266 @@
-var express = require('express')
-var bodyparser = require('body-parser')
-var mongo = require('mongodb').MongoClient
-var ObjectID = require('mongodb').ObjectID
-var dbUrl = 'mongodb://localhost:27017/swvldb'
-var app = express()
+/**
+ * This is an implementation for an authorization microservice
+ */
+/* global require */
+const express = require('express');
+const bodyparser = require('body-parser');
+const mongo = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const dbUrl = 'mongodb://localhost:27017/swvldb';
+const hasProp = Object.prototype.hasOwnProperty;
+const app = express();
 
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({extended: false}))
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: false}));
 
-function Database (url, collectionName) {
-  this.url = url
-  this.collectionName = collectionName
-}
+/** Class representing a Mongo collection. */
+class MongoCollection {
+  /**
+     * Creates a Mongo collection.
+     * @param {string} url - The url of the Mongo database.
+     * @param {string} collectionName - The name of the collection to work on.
+     */
+  constructor(url, collectionName) {
+    /** @member {string} */
+    this.url = url;
 
-Database.prototype.insert = function (doc, httpRes) {
-  mongo.connect(this.url, (err, client) => {
-    if (err) {
-      sendInternalError(httpRes)
-      throw err
-    }
-    var docs = client.db('swvldb').collection(this.collectionName)
-    docs.insert(doc, (err, result) => {
+    /** @member {string} */
+    this.collectionName = collectionName;
+  }
+
+  /**
+     * Inserts a document into the Mongo collection.
+     * @param {object} doc - The document to be inserted.
+     * @param {object} httpRes - The http response to write over.
+     */
+  insert(doc, httpRes) {
+    mongo.connect(this.url, ((err, client) => {
       if (err) {
-        sendInternalError(httpRes)
-        throw err
-      } else {
-        httpRes.writeHead(200, {'Content-Type': 'application/json'})
-        httpRes.end(JSON.stringify(result['ops'][0]))
+        sendInternalError(httpRes);
+        throw err;
       }
-      client.close()
-    })
-  })
-}
-
-Database.prototype.find = function (query, project, httpRes, callback) {
-  mongo.connect(this.url, (err, client) => {
-    if (err) {
-      sendInternalError(httpRes)
-      throw err
-    }
-    var docs = client.db('swvldb').collection(this.collectionName)
-    docs.find(query).project(project).toArray((err, result) => {
-      if (err) {
-        sendInternalError(httpRes)
-        throw err
-      } else {
-        callback(result, httpRes, arguments[4])
-      }
-      client.close()
-    })
-  })
-}
-
-Database.prototype.update = function (query, update, httpRes) {
-  mongo.connect(this.url, (err, client) => {
-    if (err) {
-      sendInternalError(httpRes)
-      throw err
-    }
-    var docs = client.db('swvldb').collection(this.collectionName)
-    docs.updateOne(query, update, (err, result) => {
-      if (err) {
-        sendInternalError(httpRes)
-        throw err
-      } else {
-        if (result['result']['n'] === 0) { // # Matched docs === 0
-          sendNotFound(httpRes)
+      let docs = client.db('swvldb').collection(this.collectionName);
+      docs.insert(doc, ((err, result) => {
+        if (err) {
+          sendInternalError(httpRes);
+          throw err;
         } else {
-          httpRes.writeHead(204)
-          httpRes.end()
+          httpRes.writeHead(200, {'Content-Type': 'application/json'});
+          httpRes.end(JSON.stringify(result.ops[0]));
         }
+        client.close();
+      }));
+    }));
+  }
+
+  /**
+     * Finds documents into the Mongo collection.
+     * @param {object} query - The query object to be run over the collection.
+     * @param {object} project - The projection object to get specific fields.
+     * @param {object} httpRes - The http response to write over.
+     * @param {function} callback - The callback function to call with the find
+      results, and any additional args.
+     * @param {string} args - Additional argument to pass to the @param callback.
+     */
+  find(query, project, httpRes, callback, args = null) {
+    mongo.connect(this.url, ((err, client) => {
+      if (err) {
+        sendInternalError(httpRes);
+        throw err;
       }
-      client.close()
-    })
-  })
+      let docs = client.db('swvldb').collection(this.collectionName);
+      docs.find(query).project(project).toArray((err, result) => {
+        if (err) {
+          sendInternalError(httpRes);
+          throw err;
+        } else {
+          callback(result, httpRes, args);
+        }
+        client.close();
+      });
+    }));
+  }
+
+  /**
+     * Updates documents into the Mongo collection.
+     * @param {object} query - The query object to be run over the collection.
+     * @param {object} update - The update query to be applied.
+     * @param {object} httpRes - The http response to write over.
+     */
+  update(query, update, httpRes) {
+    mongo.connect(this.url, ((err, client) => {
+      if (err) {
+        sendInternalError(httpRes);
+        throw err;
+      }
+      let docs = client.db('swvldb').collection(this.collectionName);
+      docs.updateOne(query, update, ((err, result) => {
+        if (err) {
+          sendInternalError(httpRes);
+          throw err;
+        } else {
+          if (result.result.n === 0) { // # Matched docs === 0
+            sendNotFound(httpRes);
+          } else {
+            httpRes.writeHead(204);
+            httpRes.end();
+          }
+        }
+        client.close();
+      }));
+    }));
+  }
 }
 
-var resources = new Database(dbUrl, 'resources')
-var groups = new Database(dbUrl, 'groups')
+const resources = new MongoCollection(dbUrl, 'resources');
+const groups = new MongoCollection(dbUrl, 'groups');
 
-function sendOneObj (result, httpRes) {
-  httpRes.writeHead(200, {'Content-Type': 'application/json'})
-  httpRes.end(JSON.stringify(result[0]))
-}
+const sendOneObj = function writeOneObj(result, httpRes) {
+  httpRes.writeHead(200, {'Content-Type': 'application/json'});
+  httpRes.end(JSON.stringify(result[0]));
+};
 
-function sendAllObj (result, httpRes) {
-  httpRes.writeHead(200, {'Content-Type': 'application/json'})
+const sendAllObj = function writeAllObj(result, httpRes) {
+  httpRes.writeHead(200, {'Content-Type': 'application/json'});
   httpRes.end(JSON.stringify({
-    'count': result.length,
-    'items': result
-  }))
-}
+    count: result.length,
+    items: result
+  }));
+};
 
-function sendUserIds (result, httpRes) {
-  httpRes.writeHead(200, {'Content-Type': 'application/json'})
+const sendUserIds = function writeUserIds(result, httpRes) {
+  httpRes.writeHead(200, {'Content-Type': 'application/json'});
   httpRes.end(JSON.stringify({
-    'count': result[0]['userIds'].length,
-    'items': result[0]['userIds']
-  }))
-}
+    count: result[0].userIds.length,
+    items: result[0].userIds
+  }));
+};
 
-function getRsourceNames (result, httpRes) {
-  var resourceIds = []
-  result[0]['resourceIds'].forEach((obj) => {
-    resourceIds.push(new ObjectID(obj.resourceId))
-  })
-  var query = { '_id': { $in: resourceIds } }
-  resources.find(query, {}, httpRes, sendAllObj)
-}
+const getRsourceNames = function qResourceNames(result, httpRes) {
+  const resourceIds = result[0].resourceIds.map(
+    obj => new ObjectID(obj.resourceId));
+  let query = { _id: { $in: resourceIds } };
+  resources.find(query, {}, httpRes, sendAllObj);
+};
 
-function isAuthorized (result, httpRes, userId) {
+const isAuthorized = function qAuthorization(result, httpRes, qUserId) {
   if (result.length === 0) {
-    sendNotFound(httpRes)
+    sendNotFound(httpRes);
   } else {
-    var query = { $and: [{ 'userIds': { $in: [{ 'userId': userId }] } },
-      { 'resourceIds': { $in: [{ 'resourceId': result[0]['_id'].toString() }] } }] }
-    var fields = {'_id': 1}
-    groups.find(query, fields, httpRes, sendAuth)
+    let query = { $and: [{ userIds: { $in: [{ userId: qUserId }] } },
+      { resourceIds: { $in: [{ resourceId: result[0]['_id'].toString() }] } }] };
+    let fields = {_id: 1};
+    groups.find(query, fields, httpRes, sendAuth);
   }
-}
+};
 
-function sendAuth (result, httpRes) {
+const sendAuth = function writeAuth(result, httpRes) {
   if (result.length > 0) {
-    httpRes.writeHead(200, {'Content-Type': 'application/json'})
-    httpRes.end(JSON.stringify({ 'authorized': true }))
+    httpRes.writeHead(200, {'Content-Type': 'application/json'});
+    httpRes.end(JSON.stringify({ authorized: true }));
   } else {
-    httpRes.writeHead(403, {'Content-Type': 'application/json'})
-    httpRes.end(JSON.stringify({ 'authorized': false }))
+    httpRes.writeHead(403, {'Content-Type': 'application/json'});
+    httpRes.end(JSON.stringify({ authorized: false }));
   }
-}
+};
 
-function sendInternalError (httpRes) {
-  httpRes.writeHead(500)
-  httpRes.end()
-}
+const sendInternalError = function send500(httpRes) {
+  httpRes.writeHead(500);
+  httpRes.end();
+};
 
-function sendNotFound (httpRes) {
-  httpRes.writeHead(404)
-  httpRes.end()
-}
+const sendNotFound = function send404(httpRes) {
+  httpRes.writeHead(404);
+  httpRes.end();
+};
 
-function sendBadReq (httpRes) {
-  httpRes.sendStatus(400)
-  httpRes.end()
-}
+const sendBadReq = function send400(httpRes) {
+  httpRes.sendStatus(400);
+  httpRes.end();
+};
 
 // POST /resource
-app.post('/resource', (req, res) => {
-  if (req.body.hasOwnProperty('name')) {
-    resources.insert({ name: req.body.name }, res)
+app.post('/resource', ((req, res) => {
+  if (hasProp.call(req.body, 'name')) {
+    resources.insert({ name: req.body.name }, res);
   } else {
-    sendBadReq(res)
+    sendBadReq(res);
   }
-})
+}));
 
 // GET /resource/:id
-app.get('/resource/:id', (req, res) => {
-  resources.find({ '_id': new ObjectID(req.params.id) }, {}, res, sendOneObj)
-})
+app.get('/resource/:id', ((req, res) => {
+  resources.find({ _id: new ObjectID(req.params.id) }, {}, res, sendOneObj);
+}));
 
 // GET /resource
-app.get('/resource/', (req, res) => {
-  resources.find({}, {}, res, sendAllObj)
-})
+app.get('/resource/', ((req, res) => {
+  resources.find({}, {}, res, sendAllObj);
+}));
 
 // POST /group
-app.post('/group', (req, res) => {
-  if (req.body.hasOwnProperty('name')) {
-    var group = { name: req.body.name }
-    if (req.body.hasOwnProperty('description')) {
-      group.description = req.body.description
+app.post('/group', ((req, res) => {
+  if (hasProp.call(req.body, 'name')) {
+    var group = { name: req.body.name };
+    if (hasProp.call(req.body, 'description')) {
+      group.description = req.body.description;
     }
-    groups.insert(group, res)
+    groups.insert(group, res);
   } else {
-    sendBadReq(res)
+    sendBadReq(res);
   }
-})
+}));
 
 // GET /group/:id
-app.get('/group/:id', (req, res) => {
-  var query = { '_id': new ObjectID(req.params.id) }
-  var fields = {'name': 1, 'description': 1}
-  groups.find(query, fields, res, sendOneObj)
-})
+app.get('/group/:id', ((req, res) => {
+  let query = { _id: new ObjectID(req.params.id) };
+  let fields = { name: 1, description: 1 };
+  groups.find(query, fields, res, sendOneObj);
+}));
 
 // GET /group
-app.get('/group/', (req, res) => {
-  groups.find({}, {'name': 1, 'description': 1}, res, sendAllObj)
-})
+app.get('/group/', ((req, res) => {
+  groups.find({}, { name: 1, description: 1 }, res, sendAllObj);
+}));
 
 // POST /group/:id/user
-app.post('/group/:id/user', (req, res) => {
-  var query = { '_id': new ObjectID(req.params.id) }
-  var update = { $addToSet: { userIds: { $each: req.body } } }
-  groups.update(query, update, res)
-})
+app.post('/group/:id/user', ((req, res) => {
+  let query = { _id: new ObjectID(req.params.id) };
+  let update = { $addToSet: { userIds: { $each: req.body } } };
+  groups.update(query, update, res);
+}));
 
 // GET /group/:id/user
-app.get('/group/:id/user', (req, res) => {
-  var query = {'_id': new ObjectID(req.params.id)}
-  var fields = {'userIds': 1, '_id': 0}
-  groups.find(query, fields, res, sendUserIds)
-})
+app.get('/group/:id/user', ((req, res) => {
+  let query = { _id: new ObjectID(req.params.id) };
+  let fields = { userIds: 1, _id: 0 };
+  groups.find(query, fields, res, sendUserIds);
+}));
 
 // POST /group/:id/authorize
-app.post('/group/:id/authorize', (req, res) => {
-  var query = {'_id': new ObjectID(req.params.id)}
-  var update = { $addToSet: { resourceIds: { $each: req.body } } }
-  groups.update(query, update, res)
-})
+app.post('/group/:id/authorize', ((req, res) => {
+  let query = { _id: new ObjectID(req.params.id)};
+  let update = { $addToSet: { resourceIds: { $each: req.body } } };
+  groups.update(query, update, res);
+}));
 
 // GET /group/:id/resource
-app.get('/group/:id/resource', (req, res) => {
-  var query = {'_id': new ObjectID(req.params.id)}
-  var fields = {'resourceIds': 1, '_id': 0}
-  groups.find(query, fields, res, getRsourceNames)
-})
+app.get('/group/:id/resource', ((req, res) => {
+  let query = { _id: new ObjectID(req.params.id) };
+  let fields = { resourceIds: 1, _id: 0 };
+  groups.find(query, fields, res, getRsourceNames);
+}));
 
 // GET /authorized?userId=&resourceName=
-app.get('/authorized', (req, res) => {
-  if (req.query.hasOwnProperty('userId') &&
-    req.query.hasOwnProperty('resourceName')) {
-    resources.find({ 'name': req.query.resourceName }, {}, res, isAuthorized,
-      req.query.userId)
+app.get('/authorized', ((req, res) => {
+  if (hasProp.call(req.query, 'userId') &&
+    hasProp.call(req.query, 'resourceName')) {
+    resources.find({ name: req.query.resourceName }, {}, res, isAuthorized,
+      req.query.userId);
   } else {
-    sendBadReq(res)
+    sendBadReq(res);
   }
-})
+}));
 
-app.listen(process.argv[2])
+/* global process */
+app.listen(process.argv[2]);
